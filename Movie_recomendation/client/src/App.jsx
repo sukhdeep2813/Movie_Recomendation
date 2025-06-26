a87e293d130d983941b6021e082ddb9a90ef3c07
sukhdeep <sukhdeepkumar918@gmail.com>
2025-06-26 16:13:32 +0530

    folder skeleton

---
+++ Movie_recomendation/client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-4xl font-bold text-center text-black-600 mt-10">
+        hello
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Movie_recomendation/client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./inde.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
