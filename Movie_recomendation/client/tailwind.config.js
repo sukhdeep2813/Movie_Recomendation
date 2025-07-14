fa9ae3eca2cf9b3cfc0de6606c81a21d98926f6e
sukhdeep <sukhdeepkumar918@gmail.com>
2025-07-14 18:38:43 +0530

    Update

--- Movie_recomendation/client/tailwind.config.js
+++ Movie_recomendation/client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./inde.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
